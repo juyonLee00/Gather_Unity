bd40b0cda34713e894c58571456f598ec06e7e93
juyonLee00 <ljy012900@gmail.com>
2023-09-06 10:45:18 +0900

    #1 [feat] 플레이어 WASD 움직임 기능 추가

--- Assets/Scripts/Controllers/TopDownCharacterController.cs
+++ Assets/Scripts/Controllers/TopDownCharacterController.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TopDownCharacterController : MonoBehaviour
 {
-    public float speed = 5f;
-    void Start()
+    //event : 외부에서 호출 못하게 막기
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
     {
-        
+        OnMoveEvent?.Invoke(direction);
     }
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+
+    /*
+    public float speed = 5f;
 
     // Update is called once per frame
     void Update()
@@ -17,5 +29,5 @@ public class TopDownCharacterController : MonoBehaviour
         float y = Input.GetAxis("Vertical");
 
         transform.position += new Vector3(x, y) * speed * Time.deltaTime;
-    }
+    }*/
 }
